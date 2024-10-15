98c426ca10bec91420b0529ab4f699966e17d43a
Johnathan Baggett <jbagg1986@gmail.com>
2024-10-15 14:51:14 -0400

    change

--- Assignment 3 Cards/MainForm.cs
+++ Assignment 3 Cards/MainForm.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace Assignment_3_Cards
 {
@@ -22,7 +17,12 @@ namespace Assignment_3_Cards
 
         //PictureBox and CheckBox arrays
         PictureBox[] pictureBoxes;
-        System.Windows.Forms.CheckBox[] keepCheckBoxes;
+        CheckBox[] keepCheckBoxes;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
 
         private void ShuffleDeck()
         {
@@ -31,10 +31,9 @@ namespace Assignment_3_Cards
             for (int i = 0; i < imageList1.Images.Count; i++)
             {
                 //Add cards to the deck list
-                deckList.Add(i); 
+                deckList.Add(i);
             }
 
- 
             Random rnd = new Random();
             int n = deckList.Count;
             while (n > 1)
@@ -45,13 +44,6 @@ namespace Assignment_3_Cards
                 deckList[k] = deckList[n];
                 deckList[n] = value;
             }
-        }
-
-
-        public MainForm()
-        {
-            InitializeComponent();
-
         }
 
         private void Card1pictureBox_Click(object sender, EventArgs e)
@@ -81,26 +73,27 @@ namespace Assignment_3_Cards
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            imageList1.ImageSize = new Size(100, 140); 
+            imageList1.ImageSize = new Size(100, 140);
 
             //Load card images into the ImageList
-            string[] files = System.IO.Directory.GetFiles("C:\\Users\\jbagg\\source\\repos\\Assignment 3 Cards\\Assignment 3 Cards\\Cards\\", "*.png"); // Adjust the file extension as needed
+            string[] files = Directory.GetFiles("C:\\Users\\jbagg\\source\\repos\\Assignment 3 Cards\\Assignment 3 Cards\\Cards\\", "*.png");
             foreach (string file in files)
             {
                 imageList1.Images.Add(Image.FromFile(file));
             }
-            //Initialize the pictureBoxes 
+
+            //Initialize the PictureBoxes 
             pictureBoxes = new PictureBox[] { Card1pictureBox, Card2pictureBox, Card3pictureBox, Card4pictureBox, Card5pictureBox };
 
-            //Initialize checkBoxes
-            keepCheckBoxes = new System.Windows.Forms.CheckBox[] { Keep1checkBox, Keep2checkBox, Keep3checkBox, Keep4checkBox, Keep5checkBox };
+            //Initialize CheckBoxes
+            keepCheckBoxes = new CheckBox[] { Keep1checkBox, Keep2checkBox, Keep3checkBox, Keep4checkBox, Keep5checkBox };
 
             //Deal the initial hand of cards
             ShuffleDeck();
             for (int i = 0; i < handArray.Length; i++)
             {
-                handArray[i] = deckList[i]; 
-                pictureBoxes[i].Image = imageList1.Images[handArray[i]]; 
+                handArray[i] = deckList[i];
+                pictureBoxes[i].Image = imageList1.Images[handArray[i]];
             }
         }
 
@@ -108,83 +101,94 @@ namespace Assignment_3_Cards
         {
             ShuffleDeck();
 
-            // Uncheck all checkboxes before dealing new cards
-            foreach (var checkBox in keepCheckBoxes)
-            {
-                //Uncheck the boxes after dealing
-                checkBox.Checked = false; 
-            }
-
             for (int i = 0; i < handArray.Length; i++)
             {
                 //Deal new card if the checkbox is not checked
                 if (!keepCheckBoxes[i].Checked)
                 {
                     //Ensure that the deck has cards left to draw
+                    if (deckList.Count < 5)
+                    {
+                        ShuffleDeck();
+                    }
+
                     if (deckList.Count > 0)
                     {
                         int cardToDeal = deckList[0];
+                        handArray[i] = cardToDeal;
+                        deckList.RemoveAt(0);
 
-                        //Check if this card has already been dealt
-                        if (!handArray.Contains(cardToDeal))
-                        {
-                            handArray[i] = cardToDeal; 
-                            deckList.RemoveAt(0);
-
-                            //Set the PictureBox to show the card
-                            pictureBoxes[i].Image = imageList1.Images[handArray[i]];
-                        }
-                        else
-                        { 
-                            i--;
-                        }
+                        //Set the PictureBox to show the card
+                        pictureBoxes[i].Image = imageList1.Images[handArray[i]];
                     }
                 }
+                else
+                {
+                    //If the card is kept, update the PictureBox with the current card in hand
+                    pictureBoxes[i].Image = imageList1.Images[handArray[i]];
+                }
+            }
+            //Uncheck the checkboxes after dealing new cards
+            foreach (var checkBox in keepCheckBoxes)
+            {
+                checkBox.Checked = false;
             }
         }
 
         private void LoadHandbutton_Click(object sender, EventArgs e)
         {
-                OpenFileDialog openFileDialog = new OpenFileDialog
-                {
-                    //Set the InitialDir and DefaultExt
-                    InitialDirectory = @"C:\",  
-                    DefaultExt = "txt"
-                };
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    string[] lines = System.IO.File.ReadAllLines(openFileDialog.FileName);
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                DefaultExt = "txt"
+            };
 
-                    for (int i = 0; i < handArray.Length; i++)
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+
+                for (int i = 0; i < handArray.Length; i++)
+                {
+                    if (i < lines.Length)
                     {
-                        if (i < lines.Length)
+                        try
                         {
-                            handArray[i] = int.Parse(lines[i]); 
-
-                            //Update the PictureBox 
+                            handArray[i] = int.Parse(lines[i]);
+                        }
+                        catch (FormatException)
+                        {
+                            handArray[i] = -1; 
+                        }
+                        if (handArray[i] >= 0 && handArray[i] < imageList1.Images.Count)
+                        {
                             pictureBoxes[i].Image = imageList1.Images[handArray[i]];
                         }
                         else
                         {
-                            handArray[i] = -1; 
+                            handArray[i] = -1; // Invalid index, no card
                             pictureBoxes[i].Image = null;
                         }
                     }
+                    else
+                    {
+                        handArray[i] = -1;
+                        pictureBoxes[i].Image = null;
+                    }
                 }
             }
+        }
 
         private void SaveHandbutton_Click(object sender, EventArgs e)
         {
-            // Create and configure the SaveFileDialog
             using (SaveFileDialog saveFileDialog = new SaveFileDialog())
             {
-                saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); ;
-                saveFileDialog.DefaultExt = "txt"; 
-                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; 
+                saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                saveFileDialog.DefaultExt = "txt";
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
 
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    //Open the file 
+                    // Open the file 
                     using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
                     {
                         for (int i = 0; i < handArray.Length; i++)
@@ -196,5 +200,4 @@ namespace Assignment_3_Cards
             }
         }
     }
-    }
-
+}
